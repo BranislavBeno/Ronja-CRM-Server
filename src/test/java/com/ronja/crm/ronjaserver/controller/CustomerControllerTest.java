252103cd807d43e6b96cd5a434c0f40c67f96df0
Branislav Beno <brano.beno@gmail.com>
2021-03-08 22:36:16 +0100

    Finish test creation for web layer

--- src/test/java/com/ronja/crm/ronjaserver/controller/CustomerControllerTest.java
+++ src/test/java/com/ronja/crm/ronjaserver/controller/CustomerControllerTest.java
@@ -1,7 +1,10 @@
 package com.ronja.crm.ronjaserver.controller;
 
+import com.ronja.crm.ronjaserver.entity.Customer;
 import com.ronja.crm.ronjaserver.service.CustomerService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -9,8 +12,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -32,14 +38,49 @@ class CustomerControllerTest {
   }
 
   @Test
-  void shouldGetEmptyArrayWhenNoBooksExists() throws Exception {
+  void testFindAll() throws Exception {
+    when(service.findAll()).thenReturn(List.of(new Customer(), new Customer()));
     this.mockMvc
         .perform(get("/customers/list")
             .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON))
         .andExpect(status().is(200))
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$.size()", is(0)))
+        .andExpect(jsonPath("$.size()", is(2)))
         .andDo(print())
         .andReturn();
+    verify(service).findAll();
+  }
+
+  @Test
+  void testSearch() throws Exception {
+    when(service.searchBy(anyString())).thenReturn(List.of(new Customer()));
+    this.mockMvc
+        .perform(get("/customers/search?customerName=Emma")
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.size()", is(1)));
+    verify(service).searchBy(anyString());
+  }
+
+  @Test
+  void testSave() throws Exception {
+    Mockito.when(service.save(any(Customer.class))).thenReturn(new Customer());
+    this.mockMvc
+        .perform(post("/customers/save")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"firstName\": \"Emma\",\"lastName\": \"Button\",\"companyName\": \"SpiceCorp\"}"))
+        .andExpect(status().isCreated())
+        .andExpect(header().exists("Content-Type"))
+        .andExpect(header().string("Content-Type", Matchers.equalTo("application/json")));
+    verify(service).save(any(Customer.class));
+  }
+
+  @Test
+  void testDelete() throws Exception {
+    service.deleteById(anyInt());
+    this.mockMvc
+        .perform(delete("/customers/delete/1"))
+        .andExpect(status().is(204));
+    verify(service, times(2)).deleteById(anyInt());
   }
 }
