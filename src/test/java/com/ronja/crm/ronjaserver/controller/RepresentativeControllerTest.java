38b3b379753750f6b208352cbc7f53d7599b4101
Branislav Beno <brano.beno@gmail.com>
2021-04-29 22:55:08 +0200

    Add unit test for representatives controller

--- src/test/java/com/ronja/crm/ronjaserver/controller/RepresentativeControllerTest.java
+++ src/test/java/com/ronja/crm/ronjaserver/controller/RepresentativeControllerTest.java
@@ -1,7 +1,7 @@
 package com.ronja.crm.ronjaserver.controller;
 
-import com.ronja.crm.ronjaserver.dto.CustomerDto;
-import com.ronja.crm.ronjaserver.entity.Customer;
+import com.ronja.crm.ronjaserver.dto.RepresentativeDto;
+import com.ronja.crm.ronjaserver.entity.Representative;
 import com.ronja.crm.ronjaserver.service.EntityService;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
@@ -20,11 +20,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(CustomerController.class)
-class CustomerControllerTest {
+@WebMvcTest(RepresentativeController.class)
+class RepresentativeControllerTest {
 
   @MockBean
-  EntityService<Customer, CustomerDto> service;
+  EntityService<Representative, RepresentativeDto> service;
 
   @Autowired
   MockMvc mockMvc;
@@ -32,16 +32,16 @@ class CustomerControllerTest {
   @Test
   void testShouldNotReturnXML() throws Exception {
     this.mockMvc
-        .perform(get("/customers/list")
+        .perform(get("/representatives/list")
             .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_XML))
         .andExpect(status().isNotAcceptable());
   }
 
   @Test
   void testFindAll() throws Exception {
-    when(service.findAll()).thenReturn(List.of(new Customer(), new Customer()));
+    when(service.findAll()).thenReturn(List.of(new Representative(), new Representative()));
     this.mockMvc
-        .perform(get("/customers/list")
+        .perform(get("/representatives/list")
             .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON))
         .andExpect(status().is(200))
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
@@ -53,9 +53,9 @@ class CustomerControllerTest {
 
   @Test
   void testSearch() throws Exception {
-    when(service.searchBy(anyString())).thenReturn(List.of(new Customer()));
+    when(service.searchBy(anyString())).thenReturn(List.of(new Representative()));
     this.mockMvc
-        .perform(get("/customers/search?companyName=Emma")
+        .perform(get("/representatives/search?lastName=Baumann")
             .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.size()", is(1)));
@@ -64,50 +64,58 @@ class CustomerControllerTest {
 
   @Test
   void testSave() throws Exception {
-    when(service.save(any(CustomerDto.class))).thenReturn(new Customer());
+    when(service.save(any(RepresentativeDto.class))).thenReturn(new Representative());
     this.mockMvc
-        .perform(post("/customers/add")
+        .perform(post("/representatives/add")
             .contentType(MediaType.APPLICATION_JSON)
             .content("""
                 {
-                     "companyName": "IdaCorp",
-                     "category": "LEVEL_3",
-                     "focus": "TRADE",
-                     "status": "ACTIVE"
+                    "firstName": "Roger",
+                    "lastName": "Patrick",
+                    "position": "CTO",
+                    "region": "ASEAN",
+                    "notice": "anything special",
+                    "status": "ACTIVE",
+                    "lastVisit": "2021-01-17",
+                    "scheduledVisit": "2021-05-05"
                 }
                 """))
         .andExpect(status().isCreated())
         .andExpect(header().exists("Content-Type"))
         .andExpect(header().string("Content-Type", Matchers.equalTo("application/json")));
-    verify(service).save(any(CustomerDto.class));
+    verify(service).save(any(RepresentativeDto.class));
   }
 
   @Test
   void testUpdate() throws Exception {
-    when(service.update(any(CustomerDto.class))).thenReturn(new Customer());
+    when(service.update(any(RepresentativeDto.class))).thenReturn(new Representative());
     this.mockMvc
-        .perform(post("/customers/update")
+        .perform(post("/representatives/update")
             .contentType(MediaType.APPLICATION_JSON)
             .content("""
                 {
-                     "id": 6,
-                     "companyName": "IdaCorp",
-                     "category": "LEVEL_3",
-                     "focus": "TRADE",
-                     "status": "ACTIVE"
+                    "id": 3,
+                    "firstName": "Roger",
+                    "lastName": "Patrick",
+                    "position": "CTO",
+                    "region": "ASEAN",
+                    "notice": "anything special",
+                    "status": "ACTIVE",
+                    "lastVisit": "2021-01-17",
+                    "scheduledVisit": "2021-05-05"
                 }
                 """))
         .andExpect(status().isCreated())
         .andExpect(header().exists("Content-Type"))
         .andExpect(header().string("Content-Type", Matchers.equalTo("application/json")));
-    verify(service).update(any(CustomerDto.class));
+    verify(service).update(any(RepresentativeDto.class));
   }
 
   @Test
   void testDelete() throws Exception {
     service.deleteById(anyInt());
     this.mockMvc
-        .perform(delete("/customers/delete/1"))
+        .perform(delete("/representatives/delete/1"))
         .andExpect(status().is(204));
     verify(service, times(2)).deleteById(anyInt());
   }
