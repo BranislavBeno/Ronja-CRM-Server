c4b3f83a34b8af60fdf95e403672d95abd27e5e0
Branislav Beno <branislav.beno@siemens.com>
2023-07-25 10:33:27 +0200

    Use @ServiceConnection

--- src/test/java/com/ronja/crm/ronjaserver/repository/CustomerRepositoryTest.java
+++ src/test/java/com/ronja/crm/ronjaserver/repository/CustomerRepositoryTest.java
@@ -3,59 +3,53 @@ package com.ronja.crm.ronjaserver.repository;
 import com.ronja.crm.ronjaserver.entity.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers(disabledWithoutDocker = true)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CustomerRepositoryTest extends BaseRepositoryTest {
 
-  @Autowired
-  CustomerRepository repository;
+    @Autowired
+    private CustomerRepository repository;
 
-  @Test
-  @Sql(scripts = "/scripts/INIT_CUSTOMERS.sql")
-  void testFindAll() {
-    List<Customer> result = (List<Customer>) repository.findAll();
-    assertThat(result).hasSize(2);
-  }
+    @Test
+    @Sql(scripts = "/scripts/INIT_CUSTOMERS.sql")
+    void testFindAll() {
+        List<Customer> result = (List<Customer>) repository.findAll();
+        assertThat(result).hasSize(2);
+    }
 
-  @Test
-  @Sql(scripts = "/scripts/INIT_CUSTOMERS.sql")
-  void testSearchBy() {
-    List<Customer> result = repository.findByCompanyNameContainsAllIgnoreCase("John");
-    assertThat(result).hasSize(1);
-    assertThat(result.get(0).getId()).isEqualTo(1);
-    assertThat(result.get(0).getCategory()).isEqualTo("LEVEL_1");
-    assertThat(result.get(0).getFocus()).isEqualTo("TRADE");
-    assertThat(result.get(0).getStatus()).isEqualTo("ACTIVE");
-    assertThat(result.get(0).getCompanyName()).isEqualTo("JohnCorp");
-  }
+    @Test
+    @Sql(scripts = "/scripts/INIT_CUSTOMERS.sql")
+    void testSearchBy() {
+        List<Customer> result = repository.findByCompanyNameContainsAllIgnoreCase("John");
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0).getId()).isEqualTo(1);
+        assertThat(result.get(0).getCategory()).isEqualTo("LEVEL_1");
+        assertThat(result.get(0).getFocus()).isEqualTo("TRADE");
+        assertThat(result.get(0).getStatus()).isEqualTo("ACTIVE");
+        assertThat(result.get(0).getCompanyName()).isEqualTo("JohnCorp");
+    }
 
-  @Test
-  @Sql(scripts = "/scripts/INIT_CUSTOMERS.sql")
-  void testDeleteById() {
-    repository.deleteById(1);
-    assertThat(repository.findAll()).hasSize(1);
-  }
+    @Test
+    @Sql(scripts = "/scripts/INIT_CUSTOMERS.sql")
+    void testDeleteById() {
+        repository.deleteById(1);
+        assertThat(repository.findAll()).hasSize(1);
+    }
 
-  @Test
-  @Sql(scripts = "/scripts/INIT_CUSTOMERS.sql")
-  void testSave() {
-    Customer customer = new Customer();
-    customer.setCategory("LEVEL_1");
-    customer.setFocus("BUILDER");
-    customer.setStatus("ACTIVE");
-    customer.setCompanyName("NewmanCorp");
-    repository.save(customer);
+    @Test
+    @Sql(scripts = "/scripts/INIT_CUSTOMERS.sql")
+    void testSave() {
+        Customer customer = new Customer();
+        customer.setCategory("LEVEL_1");
+        customer.setFocus("BUILDER");
+        customer.setStatus("ACTIVE");
+        customer.setCompanyName("NewmanCorp");
+        repository.save(customer);
 
-    assertThat(repository.findAll()).hasSize(3);
-  }
+        assertThat(repository.findAll()).hasSize(3);
+    }
 }
--- src/test/java/com/ronja/crm/ronjaserver/repository/MetalDataRepositoryTest.java
+++ src/test/java/com/ronja/crm/ronjaserver/repository/MetalDataRepositoryTest.java
@@ -4,17 +4,11 @@ import com.ronja.crm.ronjaserver.entity.MetalData;
 import jakarta.validation.ValidationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Testcontainers(disabledWithoutDocker = true)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MetalDataRepositoryTest extends BaseRepositoryTest {
 
     @Autowired
