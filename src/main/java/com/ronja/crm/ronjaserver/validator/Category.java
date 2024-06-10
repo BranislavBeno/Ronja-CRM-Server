d9d5353086fd84e2d20781b02ef7bd425b877329
Branislav Beno <brano.beno@gmail.com>
2024-06-10 13:37:57 +0200

    refactor: Remove static import

--- src/main/java/com/ronja/crm/ronjaserver/validator/Category.java
+++ src/main/java/com/ronja/crm/ronjaserver/validator/Category.java
@@ -4,14 +4,13 @@ import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
-@Target({FIELD})
-@Retention(RUNTIME)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = CategoryValidator.class)
 @Documented
 public @interface Category {
