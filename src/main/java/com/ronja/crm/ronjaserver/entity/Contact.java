90f4828c127923af5845111b7f331ea127ecb185
Branislav Beno <branislav.beno@siemens.com>
2024-12-06 19:47:45 +0100

    fix: Repair failing tests

--- src/main/java/com/ronja/crm/ronjaserver/entity/Contact.java
+++ src/main/java/com/ronja/crm/ronjaserver/entity/Contact.java
@@ -1,26 +1,5 @@
 package com.ronja.crm.ronjaserver.entity;
 
-public final class Contact {
+public record Contact(String contact, String type, boolean primary) {
 
-    private final String contact;
-    private final String type;
-    private final boolean primary;
-
-    public Contact(String contact, String type, boolean primary) {
-        this.contact = contact;
-        this.type = type;
-        this.primary = primary;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public boolean isPrimary() {
-        return primary;
-    }
 }
